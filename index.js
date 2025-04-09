7e2b94d16a57abc84384cc250b30e2fa91b8a148
Rajesh Choudhary <rajeshchoudharyt@gmail.com>
2025-04-09 21:24:58 +0530

    Add port and host

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ try {
 } catch (err) {}
 
 //
-fastify.listen({ port: 3001 }, (err, address) => {
+fastify.listen({ port: 3001, host: "0.0.0.0" }, (err, address) => {
 	if (err) {
 		fastify.log(err);
 		process.exit(1);
