4c4b5b12d76df54c98a2eddd8c448ad5bc46968d
Rajesh Choudhary <rajeshchoudharyt@gmail.com>
2025-04-09 12:40:18 +0530

    Add supabase db connection

--- utils/env.js
+++ utils/env.js
@@ -2,4 +2,6 @@ export const env = {
 	// ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY,
 	ALCHEMY_WEBSOCKET_URL:
 		"wss://eth-sepolia.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY,
+	SUPABASE_URL: process.env.SUPABASE_URL,
+	SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
 };
